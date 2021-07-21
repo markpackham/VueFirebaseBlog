9101d2eb237b70e7287ceb3ddebd58c3f1be945a
mark <markpackham1@gmail.com>
2021-07-21 14:21:23 +0100

    add Footer and Nav, https://youtu.be/ISv22NNL-aE?t=3017

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,72 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="left">
+        <div class="col-1">
+          <router-link class="header" :to="{ name: 'Home' }"
+            >VueFirebaseBlog
+          </router-link>
+          <ul>
+            <li>
+              <a href="#"><youTube class="svg-icon" /></a>
+            </li>
+            <li>
+              <a href="#"><twitter class="svg-icon" /></a>
+            </li>
+            <li>
+              <a href="#"><instagram class="svg-icon" /></a>
+            </li>
+            <li>
+              <a href="#"><linkedin class="svg-icon" /></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul>
+            <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+            <router-link class="link" :to="{ name: 'Blogs' }"
+              >Blogs</router-link
+            >
+            <!-- <router-link v-if="admin" class="link" :to="{ name: 'CreatePost' }"
+              >Create Post</router-link
+            >
+            <router-link v-if="!user" class="link" :to="{ name: 'Login' }"
+              >Login In / Register</router-link
+            > -->
+          </ul>
+        </div>
+      </div>
+      <div class="right">
+        <p>Copyright 2021 All Rights Reserved</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import youTube from "../assets/Icons/youtube-brands.svg";
+import twitter from "../assets/Icons/twitter-brands.svg";
+import instagram from "../assets/Icons/instagram-brands.svg";
+import linkedin from "../assets/Icons/linkedin-brands.svg";
+export default {
+  name: "footer-vue",
+  components: {
+    youTube,
+    twitter,
+    instagram,
+    linkedin,
+  },
+  // computed: {
+  //   user() {
+  //     return this.$store.state.user;
+  //   },
+  //   admin() {
+  //     return this.$store.state.profileAdmin;
+  //   },
+  // },
+};
+</script>
+
 <style lang="scss" scoped>
 footer {
   margin-top: auto;
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,68 @@
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <router-link class="header" :to="{ name: 'Home' }"
+          >VueFirebaseBlog</router-link
+        >
+      </div>
+      <div class="nav-links">
+        <ul v-show="!mobile">
+          <router-link class="link" to="#">Home</router-link>
+          <router-link class="link" to="#">Blogs</router-link>
+          <router-link class="link" to="#">Create Post</router-link>
+          <router-link class="link" to="#">Login/Register</router-link>
+        </ul>
+      </div>
+    </nav>
+    <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile" />
+    <transition name="mobile-nav">
+      <ul class="mobile-nav" v-show="mobileNav">
+        <router-link class="link" to="#">Home</router-link>
+        <router-link class="link" to="#">Blogs</router-link>
+        <router-link class="link" to="#">Create Post</router-link>
+        <router-link class="link" to="#">Login/Register</router-link>
+      </ul>
+    </transition>
+  </header>
+</template>
+
+<script>
+import menuIcon from "../assets/Icons/bars-regular.svg";
+export default {
+  name: "navigation",
+  components: {
+    menuIcon,
+  },
+  data() {
+    return {
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      this.mobileNav = false;
+      return;
+    },
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav;
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 header {
   background-color: #fff;
