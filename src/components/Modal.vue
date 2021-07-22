91f9ecbec0807816027e25dfee2b5ad5ad9bceda
mark <markpackham1@gmail.com>
2021-07-22 01:45:18 +0100

    add components with styles for future use

---
+++ src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template></template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 101;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 8px;
+    width: 300px;
+    padding: 40px 30px;
+    background-color: #fff;
+    p {
+      text-align: center;
+    }
+    button {
+      align-self: center;
+    }
+  }
+}
+</style>
