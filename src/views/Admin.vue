e0930ad97bb2892b0059df7627d3d85ae00b6e31
mark <markpackham1@gmail.com>
2021-08-02 16:04:43 +0100

    present user profile info, https://youtu.be/ISv22NNL-aE?t=14488

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: "admin",
+  name: "Admin",
 };
 </script>
 
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,89 @@
 <template>
-  <div class="profile"></div>
+  <div class="profile">
+    <Modal
+      v-if="modalActive"
+      :modalMessage="modalMessage"
+      v-on:close-modal="closeModal"
+    />
+    <div class="container">
+      <h2>Account Settings</h2>
+      <div class="profile-info">
+        <div class="initials">{{ $store.state.profileInitials }}</div>
+        <div class="admin-badge">
+          <adminIcon class="icon" />
+          <span>admin</span>
+        </div>
+        <div class="input">
+          <label for="firstName">First Name:</label>
+          <input type="text" id="firstName" v-model="firstName" />
+        </div>
+        <div class="input">
+          <label for="lastName">Last Name:</label>
+          <input type="text" id="lastName" v-model="lastName" />
+        </div>
+        <div class="input">
+          <label for="username">Username:</label>
+          <input type="text" id="username" v-model="username" />
+        </div>
+        <div class="input">
+          <label for="email">Email:</label>
+          <input disabled type="text" id="email" v-model="email" />
+        </div>
+        <button @click="updateProfile">Save Changes</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Modal from "../components/Modal";
+import adminIcon from "../assets/Icons/user-crown-light.svg";
 export default {
-  name: "profile",
+  name: "Profile",
+  components: {
+    Modal,
+    adminIcon,
+  },
+  data() {
+    return { modalMessage: "Changes saved!", modalActive: null };
+  },
+  methods: {
+    updateProfile() {
+      return;
+    },
+    closeModal() {
+      this.modalActive = !this.modalActive;
+    },
+  },
+  computed: {
+    firstName: {
+      get() {
+        return this.$store.state.profileFirstName;
+      },
+      set(payload) {
+        this.$store.commit("changeFirstName", payload);
+      },
+    },
+    lastName: {
+      get() {
+        return this.$store.state.profileLastName;
+      },
+      set(payload) {
+        this.$store.commit("changeLastName", payload);
+      },
+    },
+    username: {
+      get() {
+        return this.$store.state.profileUsername;
+      },
+      set(payload) {
+        this.$store.commit("changeUsername", payload);
+      },
+    },
+    email() {
+      return this.$store.state.profileEmail;
+    },
+  },
 };
 </script>
 
